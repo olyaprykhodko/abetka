fc79db7513b7a04718eda7793c5af7d7be8a4241
Olha Prykhodko <o.u.prihodko@gmail.com>
2024-09-13 18:08:41 +0300

    style: change envs naming

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,14 +14,15 @@ import { JwtModule } from './auth/jwt/jwt.module';
   imports: [
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '5432', 10),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'abetka',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_DB || 'abetka',
       models: [User, Teacher, Subject, Booking, Schedule, Review, Payment],
       autoLoadModels: true,
       synchronize: true,
+      logging: console.log,
     }),
     SequelizeModule.forFeature([
       User,
