4db0479ae5a6b5af3504addb5160b1eec9e91ecb
Olha Prykhodko <o.u.prihodko@gmail.com>
2024-09-18 22:54:01 +0300

    chore: add cookie parcer

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,29 +1,43 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { LoginUserDto } from './dto/login.dto';
 import { RegisterUserDto } from './dto/register.dto';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() loginDto: LoginUserDto) {
+  async login(@Body() loginDto: LoginUserDto, @Res() res: Response) {
     const token = await this.authService.loginUser(
       loginDto.username,
       loginDto.password,
     );
-    return token;
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({ message: 'Login successful' });
   }
 
   @Post('register')
-  async register(@Body() registerDto: RegisterUserDto) {
+  async register(@Body() registerDto: RegisterUserDto, @Res() res: Response) {
     const { user, token } = await this.authService.registerUser(
       registerDto.username,
       registerDto.email,
       registerDto.password,
       registerDto.role,
     );
-    return { user, token };
+
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(201).json({ message: 'Registration successful', user });
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
 
   app.enableCors({
     origin: '*', // FIXME: unsafe
