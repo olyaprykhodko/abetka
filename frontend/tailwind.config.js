16b3a421de5f94d2b4e641251ae35dcb1db4c40b
Olha Prykhodko <o.u.prihodko@gmail.com>
2024-09-18 22:56:10 +0300

    style: add styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#16325B',
+        secondary: '#227B94',
+        background: '#F2F4F7',
+        text: '#333333',
+        textSecondary: '#6E6E6E',
+        link: '#78B7D0',
+        success: '#27AE60',
+        error: '#E74C3C',
+        cardBackground: '#FFDC7F',
+        border: '#E0E0E0',
+      },
+    },
   },
   plugins: [],
 };
