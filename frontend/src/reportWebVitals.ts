072c879cb3559fa6243c6fb071f772c4fa3fed2d
Olha Prykhodko <o.u.prihodko@gmail.com>
2024-09-23 15:02:09 +0300

    fix: fix package import

--- frontend/src/reportWebVitals.ts
+++ frontend/src/reportWebVitals.ts
@@ -1,14 +1,10 @@
-import { ReportHandler } from 'web-vitals';
-
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+import { onLCP, onINP, onCLS } from 'web-vitals';
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    onCLS(onPerfEntry);
+    onINP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onINP(onPerfEntry);
   }
 };
 
