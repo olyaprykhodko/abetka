625fe347c28d985d38274a3e6a17efaec4a58dc9
Olha Prykhodko <o.u.prihodko@gmail.com>
2024-10-16 21:38:47 +0300

    wip: profile page added

--- frontend/src/app/profile/Sidebar.tsx
+++ frontend/src/app/profile/Sidebar.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
+
 const Sidebar: React.FC = () => {
   return (
     <>
       <div>
         <p>Налаштування профілю</p>
-        <p>Налаштування сповіщень</p>
+        <Link href="./notifications">Налаштування сповіщень</Link>
         <p>Абонементи на оплата</p>
       </div>
     </>
