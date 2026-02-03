a88b3e1d74658dfdd804b037218cc363046f60b7
Olha Prykhodko <o.u.prihodko@gmail.com>
2026-02-03 15:25:14 +0200

    refactor: update teachers list and remove unused code; enhance README and CI/CD configuration

--- frontend/src/app/teachers/page.tsx
+++ frontend/src/app/teachers/page.tsx
@@ -1,120 +1,74 @@
 'use client';
 
 import Navigation from '@/components/Navigation';
-// import Link from 'next/link';
 import Image from 'next/image';
 
-const products = [
+const teachers = [
   {
     id: 1,
-    name: 'Earthen Bottle',
+    name: 'Надія Гнатюк',
     href: '#',
-    price: '$48',
+    subject: 'Українська мова і літаратура',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg',
-    imageAlt:
-      'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
+      'https://images.pexels.com/photos/4855376/pexels-photo-4855376.jpeg',
+    imageAlt: 'woman',
   },
   {
     id: 2,
-    name: 'Nomad Tumbler',
+    name: 'Шарлотта Стеценко',
     href: '#',
-    price: '$35',
+    subject: 'Англійська мова',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg',
-    imageAlt:
-      'Olive drab green insulated bottle with flared screw lid and flat top.',
+      'https://images.pexels.com/photos/3727513/pexels-photo-3727513.jpeg',
+    imageAlt: 'woman',
   },
   {
     id: 3,
-    name: 'Focus Paper Refill',
+    name: 'Андрій Жменько',
     href: '#',
-    price: '$89',
+    subject: 'Інформатика та програмування для школярів',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-03.jpg',
+      'https://images.pexels.com/photos/7255290/pexels-photo-7255290.jpeg',
     imageAlt:
       'Person using a pen to cross a task off a productivity paper card.',
   },
   {
     id: 4,
-    name: 'Machined Mechanical Pencil',
+    name: 'Вадим Гончаренко',
     href: '#',
-    price: '$35',
+    subject: 'Історія України',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-04.jpg',
+      'https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg',
     imageAlt:
       'Hand holding black machined steel mechanical pencil with brass tip and top.',
   },
   {
     id: 5,
-    name: 'Earthen Bottle',
+    name: 'Кирило Омельченко',
     href: '#',
-    price: '$48',
+    subject: 'Географія',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg',
+      'https://images.pexels.com/photos/27661934/pexels-photo-27661934.jpeg',
     imageAlt:
       'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
   },
   {
     id: 6,
-    name: 'Nomad Tumbler',
+    name: 'Кристина Решетник',
     href: '#',
-    price: '$35',
+    subject: 'Алгебра та геометрія',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg',
+      'https://images.pexels.com/photos/27603571/pexels-photo-27603571.jpeg',
     imageAlt:
       'Olive drab green insulated bottle with flared screw lid and flat top.',
-  },
-  {
-    id: 7,
-    name: 'Focus Paper Refill',
-    href: '#',
-    price: '$89',
-    imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-03.jpg',
-    imageAlt:
-      'Person using a pen to cross a task off a productivity paper card.',
-  },
-  {
-    id: 8,
-    name: 'Machined Mechanical Pencil',
-    href: '#',
-    price: '$35',
-    imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt:
-      'Hand holding black machined steel mechanical pencil with brass tip and top.',
-  },
-  {
-    id: 9,
-    name: 'Machined Mechanical Pencil',
-    href: '#',
-    price: '$35',
-    imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt:
-      'Hand holding black machined steel mechanical pencil with brass tip and top.',
-  },
-  {
-    id: 10,
-    name: 'Machined Mechanical Pencil',
-    href: '#',
-    price: '$35',
-    imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-04.jpg',
-    imageAlt:
-      'Hand holding black machined steel mechanical pencil with brass tip and top.',
   },
 ];
 
 const Teachers: React.FC = () => {
-  // const teachers = await fetch();
   return (
     <>
-      {/* Header, Navigation */}
       <div className="bg-background relative overflow-hidden">
         <div className="relative isolate px-4 pt-1 lg:px-14 ">
-          {/* Top gradient */}
           <div
             aria-hidden="true"
             className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -135,27 +89,25 @@ const Teachers: React.FC = () => {
           <hr className="border-t border-primary" />
         </div>
 
-        {/*Main */}
-
         <div className="bg-white">
           <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <h2 className="sr-only">Products</h2>
 
             <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-              {products.map((product) => (
-                <a key={product.id} href={product.href} className="group">
+              {teachers.map((teacher) => (
+                <a key={teacher.id} href={teacher.href} className="group">
                   <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                     <Image
-                      alt={product.imageAlt}
-                      src={product.imageSrc}
+                      alt={teacher.imageAlt}
+                      src={teacher.imageSrc}
                       className="h-full w-full object-cover object-center group-hover:opacity-75"
                       width="500"
                       height="500"
                     />
                   </div>
-                  <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+                  <h3 className="mt-4 text-sm text-gray-700">{teacher.name}</h3>
                   <p className="mt-1 text-lg font-medium text-gray-900">
-                    {product.price}
+                    {teacher.subject}
                   </p>
                 </a>
               ))}
